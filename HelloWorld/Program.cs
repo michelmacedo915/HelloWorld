51751abcf2f4773cc3ec62e3afc638d0a85c04a1
Michel <michelmacedo@gmail.com>
2017-11-17 21:50:27 +1100

    Shorter version

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -15,7 +15,7 @@ namespace HelloWorld
 
         public static string CreateMessage()
         {
-            return "Hello World! New version!";
+            return "Hello World";
         }
     }
 }
